ee4e226e5c18d4ee057010ae6d0f31d5c6a9b71e
Jessica Thompson <jessicathompson00@gmail.com>
2026-02-23 15:23:01 +0000

    cleanup: use prompts.yaml, load logrpobs saved by generate_logprobs,py

--- figures/generate_figure_2.py
+++ figures/generate_figure_2.py
@@ -2,77 +2,18 @@
 Communion nouns by pretained and instruction-tuned versions of Gemma 2 2B and
 Llama 3.2 3B."""
 
+import os
+import Path
+import yaml
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
 import argparse
 from transformers import AutoTokenizer
 
-# Prompts
-good_prompts = {
-    'best_one_ever': 'What, in one word, is the best thing ever?',
-    'best_please_world': 'What is the best thing in the world? Please answer in one word only.',
-    'best_single_time': 'What, in a single word, is the best thing of all time?',
-    'greatest_please_ever': 'What is the greatest thing ever? Please answer in one word only.',
-    'greatest_single_world': 'What, in a single word, is the greatest thing in the world?',
-    'greatest_one_time': 'What, in one word, is the greatest thing of all time?',
-    'most_single_ever': 'What, in a single word, is the most good thing ever?',
-    'most_one_world': 'What, in one word, is the most good thing in the world?',
-    'most_please_time': 'What is the most good thing of all time? Please answer in one word only.',
-    'best_single_ever': 'What, in a single word, is the best thing ever?',
-    'best_please_ever': 'What is the best thing ever? Please answer in one word only.',
-    'best_one_world': 'What, in one word, is the best thing in the world?',
-    'best_single_world': 'What, in a single word, is the best thing in the world?',
-    'best_one_time': 'What, in one word, is the best thing of all time?',
-    'best_please_time': 'What is the best thing of all time? Please answer in one word only.',
-    'greatest_one_ever': 'What, in one word, is the greatest thing ever?',
-    'greatest_single_ever': 'What, in a single word, is the greatest thing ever?',
-    'greatest_one_world': 'What, in one word, is the greatest thing in the world?',
-    'greatest_please_world': 'What is the greatest thing in the world? Please answer in one word only.',
-    'greatest_single_time': 'What, in a single word, is the greatest thing of all time?',
-    'greatest_please_time': 'What is the greatest thing of all time? Please answer in one word only.',
-    'most_one_ever': 'What, in one word, is the most good thing ever?',
-    'most_please_ever': 'What is the most good thing ever? Please answer in one word only.',
-    'most_single_world': 'What, in a single word, is the most good thing in the world?',
-    'most_please_world': 'What is the most good thing in the world? Please answer in one word only.',
-    'most_one_time': 'What, in one word, is the most good thing of all time?',
-    'most_single_time': 'What, in a single word, is the most good thing of all time?',
-}
-
-bad_prompts = {
-    'worst_one_ever': 'What, in one word, is the worst thing ever?',
-    'worst_sinle_ever': 'What, in a single word, is the worst thing ever?',
-    'worst_please_ever': 'What is the worst thing ever? Please answer in one word only.',
-    'worst_one_world': 'What, in one word, is the worst thing in the world?',
-    'worst_single_world': 'What, in a single word, is the worst thing in the world?',
-    'worst_please_world': 'What is the worst thing in the world? Please answer in one word only.',
-    'worst_one_time': 'What, in one word, is the worst thing of all time?',
-    'worst_single_time': 'What, in a single word, is the worst thing of all time?',
-    'worst_please_time': 'What is the worst thing of all time? Please answer in one word only.',
-    'bad_one_ever': 'What, in one word, is the most bad thing ever?',
-    'bad_single_ever': 'What, in a single word, is the most bad thing ever?',
-    'bad_please_ever': 'What is the most bad thing ever? Please answer in one word only.',
-    'bad_one_world': 'What, in one word, is the most bad thing in the world?',
-    'bad_single_world': 'What, in a single word, is the most bad thing in the world?',
-    'bad_please_world': 'What is the most bad thing in the world? Please answer in one word only.',
-    'bad_one_time': 'What, in one word, is the most bad thing of all time?',
-    'bad_single_time': 'What, in a single word, is the most bad thing of all time?',
-    'bad_please_time': 'What is the most bad thing of all time? Please answer in one word only.',
-    'terrible_one_ever': 'What, in one word, is the most terrible thing ever?',
-    'terrible_single_ever': 'What, in a single word, is the most terrible thing ever?',
-    'terrible_please_ever': 'What is the most terrible thing ever? Please answer in one word only.',
-    'terrible_one_world': 'What, in one word, is the most terrible thing in the world?',
-    'terrible_single_world': 'What, in a single word, is the most terrible thing in the world?',
-    'terrible_please_world': 'What is the most terrible thing in the world? Please answer in one word only.',
-    'terrible_one_time': 'What, in one word, is the most terrible thing of all time?',
-    'terrible_single_time': 'What, in a single word, is the most terrible thing of all time?',
-    'terrible_please_time': 'What is the most terrible thing of all time? Please answer in one word only.',
-}
-# Colors
-sns.set_context('talk')
-google_red = "#DB4437"
-meta_blue = "#0064E0"
-palette = sns.color_palette([google_red, meta_blue])
+SCRIPT_ROOT = Path(__file__).parent
+REPO_ROOT = SCRIPT_ROOT.parent
+CONFIG_DIR = REPO_ROOT / "config"
 
 # Settings
 parser = argparse.ArgumentParser()
@@ -98,7 +39,12 @@ else:
     qwen_name = 'Qwen 2.5 3B'
     y_text = (30, 25)
     sig_stars = ['**', '**', '**', '**']
+    
+# Load prompts
+with open(CONFIG_DIR / "prompts.yaml", "r") as f:
+        prompts = yaml.safe_load(f)
 
+# Get list of tokens shared by both tokenizers
 tok_g = AutoTokenizer.from_pretrained(gemma_id)
 tok_l = AutoTokenizer.from_pretrained(llama_id)
 vocab_g = set(tok_g.decode(id) for id in tok_g.vocab.values())
@@ -111,51 +57,48 @@ big2['in_vocab'] = big2.token_decoded.apply(lambda x: x in shared_tokens)
 big2 = big2.query("in_vocab==True")
 
 # Load logprobs for either pretrained or instruction tuned
-gemma_lp = pd.read_csv('../data/logprobs/[filename].csv')
-llama_lp = pd.read_csv('../data/logprobs/[filename].csv')
+repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if args.instruct:
+    llama_file = os.path.join(repo_root, 'data', 'logprobs', 'meta-llama--Llama-3.2-3B-Instruct.csv')
+    gemma_file = os.path.join(repo_root, 'data', 'logprobs', 'google--gemma-2-2b-it.csv')
+else:
+    llama_file = os.path.join(repo_root, 'data', 'logprobs', 'meta-llama--Llama-3.2-3B.csv')
+    gemma_file = os.path.join(repo_root, 'data', 'logprobs', 'google--gemma-2-2b.csv.csv')
+try:
+    gemma_lp = pd.read_csv(gemma_file)
+    llama_lp = pd.read_csv(llama_file)
+except FileNotFoundError:
+    print(f"{gemma_file} or {llama_file} not found.")
+    print("Ensure you run scripts/generate_logrpobs.py first.")
 
 # For each prompt, get rank of logprob for each token in the big2 list
+def is_good(key):
+    if 'greatest' in key or 'best' in key or 'good' in key:
+        return True
+    else:
+        return False
 dfs = []
-for key in good_prompts:
-    dfg = big2.copy()
-    assert (dfg.token_decoded.apply(lambda x: tok_g(x)['input_ids']).apply(len) == 2).all()
-    dfg['tok_idx'] = dfg.token_decoded.apply(lambda x: tok_g(x)['input_ids'][-1])
-    dfg['logprob'] = dfg.tok_idx.apply(lambda x: gemma_lp[f'{key}_lp'][x])
-    dfg['rank'] = dfg.logprob.rank(method='average', ascending=False)
-    dfg['model'] = gemma_name
-    dfg['prompt']= key
-    dfg['valence'] = 'good'
-    dfl = big2.copy()
-
-    assert (dfl.token_decoded.apply(lambda x: tok_l(x)['input_ids']).apply(len) ==2).all()
-    dfl['tok_idx'] = dfl.token_decoded.apply(lambda x: tok_l(x)['input_ids'][-1])
-    dfl['logprob'] = dfl.tok_idx.apply(lambda x: llama_lp[f'{key}_lp'][x])
-    dfl['rank'] = dfl.logprob.rank(method='average', ascending=False)
-    dfl['model'] = llama_name
-    dfl['prompt']= key
-    dfl['valence'] = 'good'
-    dfs.extend([dfg, dfl])
+for key in prompts:
+    for tok, lp in zip([tok_g, tok_l], [gemma_lp, llama_lp]):
+        df = big2.copy()
+        assert (df.token_decoded.apply(lambda x: tok(x)['input_ids']).apply(len) == 2).all()
+        df['tok_idx'] = df.token_decoded.apply(lambda x: tok(x)['input_ids'][-1])
+        df['logprob'] = df.tok_idx.apply(lambda x: lp[f'{key}_lp'][x])
+        df['rank'] = df.logprob.rank(method='average', ascending=False)
+        df['model'] = gemma_name
+        df['prompt']= key
+        df['valence'] = 'good' if is_good(key) else 'bad'
+        
+        dfs.append(df)
     
-for key in bad_prompts:
-    dfg = big2.copy()
-    dfg['tok_idx'] = dfg.token_decoded.apply(lambda x: tok_g(x)['input_ids'][-1])
-    dfg['logprob'] = dfg.tok_idx.apply(lambda x: gemma_lp[f'{key}_lp'][x])
-    dfg['rank'] = dfg.logprob.rank(method='average', ascending=False)
-    dfg['model'] = gemma_name
-    dfg['prompt']= key
-    dfg['valence'] = 'bad'
-    dfl = big2.copy()
-    dfl['tok_idx'] = dfl.token_decoded.apply(lambda x: tok_l(x)['input_ids'][-1])
-    dfl['logprob'] = dfl.tok_idx.apply(lambda x: llama_lp[f'{key}_lp'][x])
-    dfl['rank'] = dfl.logprob.rank(method='average', ascending=False)
-    dfl['model'] = llama_name
-    dfl['prompt']= key
-    dfl['valence'] = 'bad'
-
-    dfs.extend([dfg, dfl])
 big2_all_prompts = pd.concat(dfs)
 
 # Plot
+sns.set_context('talk')
+google_red = "#DB4437"
+meta_blue = "#0064E0"
+palette = sns.color_palette([google_red, meta_blue])
+
 alpha=0.7
 hue_order = [gemma_name, llama_name]
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 6), sharex=True)
